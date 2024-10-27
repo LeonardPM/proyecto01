6a3b0cf31690221727531d6f8fd8a6ce9f2dd5ca
LeonardPM <leonardpaccham@gmail.com>
2024-10-27 01:17:28 -0500

    testing to mic

--- proyecto01/src/Main.java
+++ proyecto01/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ejemplo de Java");
-        System.out.println("Manejado desde GitHub");
-        System.out.println("Versionado a través de Git");
-        System.out.println("Creado por <ubicar su usuario de github>");}}
+        int v2 = 2 + 2;
+        System.out.println("Manejado desde GitHub " + v2);
+        System.out.println("Versionado a través de Git español");
+        System.out.println("Creado por <ubicar su usuario de GitHub>");
+    }
+}
